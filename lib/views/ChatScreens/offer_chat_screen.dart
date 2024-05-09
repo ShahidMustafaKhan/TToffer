a2dfa3a0ce00a03c6cdb0a9299d676f559f71409
UmairSaqibBhutta <umairbhutta161@gmail.com>
2024-05-09 14:23:05 +0500

    code changes

--- lib/views/ChatScreens/offer_chat_screen.dart
+++ lib/views/ChatScreens/offer_chat_screen.dart
@@ -106,6 +106,7 @@ class _OfferChatScreenState extends State<OfferChatScreen> {
 
     // log("chatApi data = ${chatApi.conversationData}");
     return Scaffold(
+      key: navigatorKey,
       appBar: ChatAppBar(
         img: widget.userImgUrl,
         title: widget.title,
@@ -655,6 +656,8 @@ class CallButtonWidget extends StatelessWidget {
     log("receiver id = $id");
     log("receiver name = $name");
     return ZegoSendCallInvitationButton(
+      key: navigatorKey,
+
       // borderRadius: 30,
       iconSize: const Size.fromRadius(15),
       // buttonSize: Size.fromWidth(20),
