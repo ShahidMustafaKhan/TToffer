0184e006163a73d49a9035e1e5762370d56e66af
M.AHSAN <ahsanejaz99@gmail.com>
2024-05-16 06:36:26 +0500

    noticationapi

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     Future.delayed(const Duration(seconds: 2), () {
-      pushReplacement(context, OnBoardScreen());
+      pushReplacement(context, const OnBoardScreen());
     });
   }
 
