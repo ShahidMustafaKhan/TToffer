b68898f268f8e986eb22efffded6427df9ab07e4
M.AHSAN <ahsanejaz99@gmail.com>
2024-05-17 11:19:50 +0500

    noticationapi

--- lib/views/All Categories/all_caetgories.dart
+++ lib/views/All Categories/all_caetgories.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tt_offer/Controller/APIs%20Manager/product_api.dart';
 import 'package:tt_offer/Utils/resources/res/app_theme.dart';
 import 'package:tt_offer/Utils/widgets/others/app_text.dart';
 import 'package:tt_offer/Utils/widgets/others/custom_app_bar.dart';
 import 'package:tt_offer/views/All%20Categories/catagory_container.dart';
+import 'package:tt_offer/views/All%20Categories/category_products.dart';
+
+import '../../utils/utils.dart';
 
 class AllCategories extends StatefulWidget {
   final data;
+
   const AllCategories({super.key, this.data});
 
   @override
@@ -15,6 +21,8 @@ class AllCategories extends StatefulWidget {
 class _AllCategoriesState extends State<AllCategories> {
   @override
   Widget build(BuildContext context) {
+    final apiProvider = Provider.of<ProductsApiProvider>(context);
+
     return Scaffold(
       backgroundColor: AppTheme.whiteColor,
       appBar: const CustomAppBar1(
@@ -32,10 +40,20 @@ class _AllCategoriesState extends State<AllCategories> {
           itemBuilder: (context, index) {
             Color color = Color(int.parse(
                 widget.data[index]["color"].replaceFirst('#', '0xFF')));
-            return CatagoryContainer(
-              color: color,
-              img: "${widget.data[index]["image"]}",
-              txt: "${widget.data[index]["name"]}",
+            return InkWell(
+              onTap: () {
+                push(
+                    context,
+                    CatagoryProductScreen(
+                      catId: apiProvider.catagoryData[index]["id"],
+                      catNAme: "${apiProvider.catagoryData[index]["name"]}",
+                    ));
+              },
+              child: CatagoryContainer(
+                color: color,
+                img: "${widget.data[index]["image"]}",
+                txt: "${widget.data[index]["name"]}",
+              ),
             );
           },
         ),
